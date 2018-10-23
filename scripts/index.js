31cefbbdfd7bd2a018a8452e997bfd42bbc3eb0c
Evan Procter <evanprocter@gmail.com>
2018-10-23 15:57:16 -0400

    toggles dropdown box when user clicks on it

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
+// When the user clicks on the button,
+// toggle between hiding and showing
+// the dropdown content
+function dropDownContent() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
 console.log('I think this works...');
