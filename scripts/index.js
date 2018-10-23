25acb015670585332ed7331c358c85e2452a87e7
Evan Procter <evanprocter@gmail.com>
2018-10-23 17:45:01 -0400

    fixed error on line 27

--- scripts/index.js
+++ scripts/index.js
@@ -5,4 +5,20 @@ function dropDownContent() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+// Close the dropdown menu if the user
+// clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; io++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
 console.log('I think this works...');
