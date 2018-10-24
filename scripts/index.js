c8b3c0665f9bda513a94474e1a48d21d3275f3ad
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-24 11:55:42 -0400

    added the logic to grab the api info we need

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,26 @@
 // toggle between hiding and showing
 // the dropdown content
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+	document.getElementById('myDropdown').classList.toggle('show');
 }
 
 // Close the dropdown menu if the user
 // clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName('dropdown-content');
+		var i;
+		for (i = 0; i < dropdowns.length; io++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+};
 
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; io++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
-console.log('I think this works...');
+let pulledData;
+fetch('https://api.sportradar.us/ncaafb-t1/2018/reg/9/schedule.json?api_key=73r466k6j37hj93act4p9nwn')
+	.then((res) => res.json())
+	.then((data) => (pulledData = data))
+	.then(() => console.log(pulledData));
