192dfe4da9e45fbcb45458abbe5bcc4f4552699c
Evan Procter <evanprocter@gmail.com>
2018-10-25 13:57:19 -0400

    minimizes options as you type for a team

--- scripts/index.js
+++ scripts/index.js
@@ -20,7 +20,7 @@ function myFunction() {
 	// declare variable
 	var input, filter, ul, li, a, i;
 	input = document.getElementById('myInput');
-	filter = input.nodeValue.toUpperCase();
+	filter = input.value.toUpperCase();
 	ul = document.getElementById('myUL');
 	li = ul.getElementsByTagName('li');
 
@@ -29,7 +29,7 @@ function myFunction() {
 	// match the search query
 	for (i = 0; i < li.length; i++) {
 		a = li[i].getElementsByTagName("a")[0];
-		if (a.innerHTML.toUpperCase() .indexOf(filter) > -1) {
+		if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
 			li[i].style.display = "";
 		} else {
 			li[i].style.display = "none";
