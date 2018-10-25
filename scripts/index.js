a9ca0574383603b53d9ddabcd8b5d112e29ceac6
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-25 10:12:23 -0400

    converted arrow function terminology to clarify.

--- scripts/index.js
+++ scripts/index.js
@@ -21,11 +21,19 @@ window.onclick = function(event) {
 };
 
 let pulledData;
-fetch('https://my-little-cors-proxy.herokuapp.com/https://api.fantasydata.net/v3/cfb/stats/JSON/Teams', {
+fetch('https://my-little-cors-proxy.herokuapp.com/https://api.fantasydata.net/v3/cfb/stats/json/GamesByWeek/2018/9', {
 	headers: {
 		'Ocp-Apim-Subscription-Key': 'c72fe47dccf64d6f932fcfaa1c3bbc47'
 	}
 })
-	.then((res) => res.json())
-	.then((data) => (pulledData = data))
+	.then((whatsFetched) => whatsFetched.json()) //converts to .JSON
+	.then((convertedWhatsFetched) => (pulledData = convertedWhatsFetched))
 	.then(() => console.log(pulledData));
+
+// .then(() => console.log(pulledData[34]))
+// .then(() => console.log(pulledData[34].AwayTeamName));
+
+function getHomeTeamById() {
+	// when user chooses a team name from drop-down returns team id
+	// returns HomeTeamID
+}
