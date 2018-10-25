6a08aad4fdc9c11f7ec123b92d154f6e63a0127f
Evan Procter <evanprocter@gmail.com>
2018-10-25 11:16:57 -0400

    removed dropdown box from html js and css

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,3 @@
-// When the user clicks on the button,
-// toggle between hiding and showing
-// the dropdown content
-function myFunction() {
-	document.getElementById('myDropdown').classList.toggle('show');
-}
-
-// Close the dropdown menu if the user
-// clicks outside of it
-window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-		var dropdowns = document.getElementsByClassName('dropdown-content');
-		var i;
-		for (i = 0; i < dropdowns.length; io++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-};
-
 let pulledData;
 fetch('https://my-little-cors-proxy.herokuapp.com/https://api.fantasydata.net/v3/cfb/stats/json/GamesByWeek/2018/9', {
 	headers: {
