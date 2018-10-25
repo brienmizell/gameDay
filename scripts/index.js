3cba9432430071c348648c854be4865bc501a791
Evan Procter <evanprocter@gmail.com>
2018-10-25 12:17:09 -0400

    created loop to loop through list

--- scripts/index.js
+++ scripts/index.js
@@ -23,4 +23,16 @@ function myFunction() {
 	filter = input.nodeValue.toUpperCase();
 	ul = document.getElementById('myUL');
 	li = ul.getElementsByTagName('li');
+
+	// loop through all list items, 
+	// and hide those whoe dont
+	// match the search query
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName("a")[0];
+		if (a.innerHTML.toUpperCase() .indexOf(filter) > -1) {
+			li[i].style.display = "";
+		} else {
+			li[i].style.display = "none";
+		}
+	}
 }
