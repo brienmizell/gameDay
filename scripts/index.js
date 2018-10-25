f39e5ee42725faf96b241e765f675b98bd8508e9
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-25 16:03:31 -0400

    home teams are posting to html, but in the drop down box instead of page

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 // When the user clicks on the button,
 // toggle between hiding and showing
 // the dropdown content
+const sportsAPI =
+	'https://my-little-cors-proxy.herokuapp.com/https://api.fantasydata.net/v3/cfb/stats/json/GamesByWeek/2018/9';
+
+const gameListingArea = document.querySelector('[games-this-week]');
+
 function myFunction() {
 	document.getElementById('myDropdown').classList.toggle('show');
 }
@@ -11,7 +16,7 @@ window.onclick = function(event) {
 	if (!event.target.matches('.dropbtn')) {
 		var dropdowns = document.getElementsByClassName('dropdown-content');
 		var i;
-		for (i = 0; i < dropdowns.length; io++) {
+		for (i = 0; i < dropdowns.length; i++) {
 			var openDropdown = dropdowns[i];
 			if (openDropdown.classList.contains('show')) {
 				openDropdown.classList.remove('show');
@@ -21,14 +26,15 @@ window.onclick = function(event) {
 };
 
 let pulledData;
-fetch('https://my-little-cors-proxy.herokuapp.com/https://api.fantasydata.net/v3/cfb/stats/json/GamesByWeek/2018/9', {
+fetch(sportsAPI, {
 	headers: {
 		'Ocp-Apim-Subscription-Key': 'c72fe47dccf64d6f932fcfaa1c3bbc47'
 	}
 })
 	.then((whatsFetched) => whatsFetched.json()) //converts to .JSON
-	.then((convertedWhatsFetched) => (pulledData = convertedWhatsFetched))
-	.then(() => console.log(pulledData));
+	// .then((convertedWhatsFetched) => (pulledData = convertedWhatsFetched))
+	.then(convertGamesToElement);
+// .then(() => console.log(pulledData));
 
 // .then(() => console.log(pulledData[34]))
 // .then(() => console.log(pulledData[34].AwayTeamName));
@@ -37,3 +43,47 @@ function getHomeTeamById() {
 	// when user chooses a team name from drop-down returns team id
 	// returns HomeTeamID
 }
+const section = document.querySelector('[data-home-team]');
+
+function convertGamesToElement(gameData) {
+	console.log(section);
+	gameData.forEach((element) => {
+		let homeTeamName = element.HomeTeamName;
+		let homeTeamScore = element.HomeTeamScore;
+		let awayTeamName = element.AwayTeamName;
+		let awayTeamScore = element.AwayTeamScore;
+
+		let gameDiv = document.createElement('div');
+		gameDiv.classList.add('eachGame');
+
+		let homeTeamDiv = document.createElement('p');
+		homeTeamDiv.classList.add(homeTeamName.split(' ').join('-'));
+		gameDiv.appendChild(homeTeamDiv);
+
+		let awayTeamDiv = document.createElement('div');
+		awayTeamDiv.classList.add(awayTeamName.split(' ').join('-'));
+		gameDiv.appendChild(awayTeamDiv);
+		let gameDataDiv = document.createElement('div');
+		gameDataDiv.classList.add('gameDataDiv');
+		gameDiv.appendChild(gameDataDiv);
+
+		homeTeamDiv.textContent = homeTeamName;
+		section.appendChild(homeTeamDiv);
+		console.log(homeTeamDiv.textContent);
+
+		// console.log(homeTeamName.class);
+		// homeGameDiv.classList.add(homeTeamName.split(' ').join('-'));
+		// document.body.appendChild(homeGameDiv);
+		// console.log(homeGameDiv);
+	});
+	// awayGameDiv.classList.add('awayteam');
+	// homeGameDiv.innerHTML = (homeTeamName, awayGameDiv);
+}
+
+// for each game there needs to be a
+// - away team
+// - away team score
+// - home team
+// - home team score
+// location of game
+// start time of game
