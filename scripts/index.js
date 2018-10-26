b1a7e192e877dabf9b366d330dd53d8fa967581e
Evan Procter <evanprocter@gmail.com>
2018-10-26 15:14:13 -0400

    removed dropdown box function from js

--- scripts/index.js
+++ scripts/index.js
@@ -6,25 +6,6 @@ const sportsAPI =
 
 const gameListingArea = document.querySelector('[games-this-week]');
 
-function myFunction() {
-	document.getElementById('myDropdown').classList.toggle('show');
-}
-
-// // Close the dropdown menu if the user
-// // clicks outside of it
-// window.onclick = function(event) {
-// 	if (!event.target.matches('.theAwayTeam', '.theHomeTeam')) {
-// 		var dropdowns = document.getElementsByClassName('dropdown-content');
-// 		var i;
-// 		for (i = 0; i < dropdowns.length; i++) {
-// 			var openDropdown = dropdowns[i];
-// 			if (openDropdown.classList.contains('show')) {
-// 				openDropdown.classList.remove('show');
-// 			}
-// 		}
-// 	}
-// };
-
 let pulledData;
 fetch(sportsAPI, {
 	headers: {
